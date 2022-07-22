479117194f6bd4f1c6644d4873ba7a90db1cb11c
YANNのLAPTOP\yann <yann_xu@outlook.com>
2022-07-22 15:32:38 +0800

    🏗️ chore: update to vue3+ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new App().$mount()
+import { createSSRApp } from "vue";
+import App from "./App.vue";
+export function createApp() {
+  const app = createSSRApp(App);
+  return {
+    app,
+  };
+}
